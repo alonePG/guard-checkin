6caeb1cbcfc9124b8e417e8f580e1c14f6f207f1
alonePG <97418816+alonePG@users.noreply.github.com>
2025-07-02 11:15:58 +0700

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 // 🔧 กำหนด URL ของ Google Apps Script Web App
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbyv0GfVjQH1HINGpbGhKr-4C1PoItAKHQZLb10ztTVs3LoomLJYVhxr2X1pc8LlGezD/exec";
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxqD8eQxA6vwvOYpBi3aJ8xbjzUTHEgldxA8rPTKlTQ-uqquYpEiysTKlu4f-rVNriH/exec";
