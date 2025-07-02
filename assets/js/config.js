0b93777fe209f5ea9c2ef132b2fab340bd62dad0
alonePG <97418816+alonePG@users.noreply.github.com>
2025-07-02 13:00:26 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 // 🔧 กำหนด URL ของ Google Apps Script Web App
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbxqD8eQxA6vwvOYpBi3aJ8xbjzUTHEgldxA8rPTKlTQ-uqquYpEiysTKlu4f-rVNriH/exec";
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbziSF9wFXg8hlEYy4icPVhImp8E3mnEG_igzLb15QE6FW8ry7e2l2WZ0Zj53CwJKLsX/exec";
