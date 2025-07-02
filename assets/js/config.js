e5181f1f58872a72d01281f29922639defd14ac6
alonePG <97418816+alonePG@users.noreply.github.com>
2025-07-02 15:19:36 +0700

    Delete assets/js/config.js

--- assets/js/config.js
+++
@@ -1,2 +0,0 @@
-// 🔧 กำหนด URL ของ Google Apps Script Web App
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbzcCUTob7Ws_7VClYrPFQtLvPN8XZzcrhwATPDRA-qtTiPTh0TcA9S1hKhZF9vHC3f6/exec";
